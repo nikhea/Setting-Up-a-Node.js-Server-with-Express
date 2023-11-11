475b85d5a07e2e261ec261e100d08081080d6e22
nikhea <imonikheaugbodaga@gmail.com>
2023-11-11 11:55:43 +0100

    created port and app server has started

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
